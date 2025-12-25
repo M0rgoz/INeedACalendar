3117f9604714b8e464a606f8c2a9f31ba48fb9e8
Morgoz <morgoz@inbox.ru>
2025-12-26 02:21:20 +0300

    alpha 1.0

---
+++ main.js
@@ -0,0 +1,7 @@
+const { app, BrowserWindow } = require('electron')
+function createWindow () {
+  const win = new BrowserWindow({ width: 1200, height: 800 })
+  win.loadFile('zentime.html')
+}
+app.whenReady().then(createWindow)
+// 3.  Terminalden `npm install electron` ve `npx electron .` komutlarını çalıştırarak uygulamanızı başlatabilirsiniz.
