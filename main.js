50a300eb9e70357bcc7d87ed6d3bdeb737b08c4b
Morgoz <morgoz@inbox.ru>
2025-12-26 13:44:06 +0300

    Alpha v1.1- New UI

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const { app, BrowserWindow } = require('electron')
 function createWindow () {
   const win = new BrowserWindow({ width: 1200, height: 800 })
+  win.removeMenu();
   win.loadFile('zentime.html')
 }
 app.whenReady().then(createWindow)
